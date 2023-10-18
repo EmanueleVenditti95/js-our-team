9a4048a015b14186b72cf14ff61f238415fca922
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-18 14:58:45 +0200

    Add - array with members

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log('Our Team')
+
+// - creare gli oggetti corrispondenti ai membri del team
+const member1 = {
+    name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    photo : 'wayne-barnett-founder-ceo.jpg'
+} 
+
+const member2 = {
+    name : 'Angela Caroll',
+    role : 'Chief Editor',
+    photo : 'angela-caroll-chief-editor.jpg'
+}
+
+const member3 = {
+    name : 'Walter Gordon',
+    role : 'Office Manager',
+    photo : 'walter-gordon-office-manager.jpg'
+}
+
+const member4 = {
+    name : 'Angela Lopez',
+    role : 'Social Media Manager',
+    photo : 'angela-lopez-social-media-manager.jpg'
+}
+
+const member5 = {
+    name : 'Scott Estrada',
+    role : 'Developer',
+    photo : 'scott-estrada-developer.jpg'
+}
+
+const member6 = {
+    name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    photo : 'barbara-ramos-graphic-designer.jpg'
+}
+
+console.log(member1,member2,member3,member4,member5,member6)
+
+// - creare un array con tutti i membri
+const teamMembers = [member1,member2,member3,member4,member5,member6]
+console.log(teamMembers)
